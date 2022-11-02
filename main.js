e7bb39c6e4b3d0ecf5b4b01ee07bd8009841c1f5
Marko Ilic <ivanstojanovic00@gmail.com>
2022-11-02 23:41:21 +0100

    pseudocode added

---
+++ main.js
@@ -0,0 +1,9 @@
+//hard code question "What is the capital of ${data.country}"
+//GET request for data of cities let data = await res.json()
+//for the question pull the city and country randomly together
+//for the answers pull 3 random cities
+//after getting the JSON 
+// let cities = []
+//Math.random() * 3
+
+//https://countriesnow.space/api/v0.1/countries/cities this is country names API

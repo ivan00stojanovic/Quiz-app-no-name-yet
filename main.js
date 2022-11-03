755c548195b6843935db39b4b70e9e467bb60011
Marko Ilic <ivanstojanovic00@gmail.com>
2022-11-03 22:35:53 +0100

    basic styling done

--- main.js
+++ main.js
@@ -7,3 +7,18 @@
 //Math.random() * 3
 
 //https://countriesnow.space/api/v0.1/countries/cities this is country names API
+
+async function apiRequest(){
+    try{
+        const response = await fetch(`https://countriesnow.space/api/v0.1/countries/population/cities`)
+        const data = await response.json()
+        const oneSet = data.data
+        let cities;
+        let countries;
+        console.log(oneSet)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+apiRequest()

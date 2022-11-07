e986233f259421c833a3dbe05a8792681607007e
Marko Ilic <ivanstojanovic00@gmail.com>
2022-11-07 02:43:18 +0100

    added buttons, h3 visibility logic

--- main.js
+++ main.js
@@ -6,19 +6,60 @@
 // let cities = []
 //Math.random() * 3
 
+const button = document.getElementById('btn')
+let countriesOnly = [];
+let capitalsOnly = [];
+
+
 //https://countriesnow.space/api/v0.1/countries/cities this is country names API
 
 async function apiRequest(){
     try{
-        const response = await fetch(`https://countriesnow.space/api/v0.1/countries/population/cities`)
+        //fetch data from URL
+        const response = await fetch(`https://countriesnow.space/api/v0.1/countries/capital`)
         const data = await response.json()
-        const oneSet = data.data
-        let cities;
-        let countries;
-        console.log(oneSet)
+        let list = data.data
+
+        let countries = list.reduce((acc, cur) => {
+            acc[cur.name] = cur;
+            return acc;
+        }, [])
+
+        let capitals = list.reduce((acc, cur) => {
+            acc[cur.capital] = cur;
+            return acc;
+        }, [])
+        
+        countries = Object.keys(countries).sort()//choose 1 random
+        capitals = Object.keys(capitals).sort()//from this arry choose 3 random As
+
+         countriesOnly = countries;
+         printData(countries[Math.floor(Math.random() * 251)])
+        
     }catch(error){
         console.log(error)
     }
 }
 
-apiRequest()
+ const printData = country => {
+     document.getElementById('h1').innerText = `What is the capital of ${country}?`
+     const h3 = document.getElementById('h3');
+     const answers = document.getElementById('answers')
+     h3.classList.add('visibility')
+     answers.classList.remove('visibility')
+     
+ }
+
+
+button.addEventListener('click', apiRequest)
+
+
+
+
+//countriesOnly array that has all countries
+//populate 1 random country from that array -> push it to 
+//citiesOnly array with all capitals
+//populate 3 random capitals and print them to the DOM
+//loop through countries array that arranges items by country name
+//and match to find the already randomly populated country name and
+//
